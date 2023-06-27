e57c974f9f14c5dedaa2f5be0de4dfda0822d80d
LeeHeejun <dandoc3370@gmail.com>
2023-06-27 14:47:59 +0900

    day 2-1

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,17 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce;
     public Rigidbody rigid;
     private bool isJump;
+    private bool isWalk;
     public gameManager GM;
+
+    private Animator playerAnimator;
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
         isJump = false;
+        isWalk = false;
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,25 +27,52 @@ public class PlayerMovement : MonoBehaviour
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        if (Input.GetKeyDown(KeyCode.Space) && !isJump)
+
+        Vector3 dir = new Vector3(h, 0, v);
+
+        rigid.AddForce(dir * speed, ForceMode.Impulse);
+
+        if (h != 0 || v != 0)
         {
-            rigid.AddForce(new Vector3(0, 1, 0) * jumpForce, ForceMode.Impulse);
-            isJump = true;
+            isWalk = true;
+            playerAnimator.SetBool("isWalk", isWalk);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime);
+
+        }
+        else if(h == 0 && v == 0)
+        {
+            isWalk = false;
+            playerAnimator.SetBool("isWalk", isWalk);
         }
 
-        rigid.AddForce(new Vector3(h, 0, v) * speed, ForceMode.Impulse);  
+        if (Input.GetKeyDown(KeyCode.Space) && !isJump)
+        {
+            isJump = true;
+            playerAnimator.SetBool("isJump", isJump);
+            isWalk = false;
+            playerAnimator.SetBool("isWalk", isWalk);
+            rigid.AddForce(new Vector3(0, 1, 0) * jumpForce, ForceMode.Impulse);
+        }
+
+        
     }
 
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Floor")
-            isJump = false;
-        /*if (collision.gameObject.tag == "Item")
         {
-            Destroy(collision.gameObject);
-            score += 10;
-        }*/
+            isJump = false;
+            isWalk = false;
+            playerAnimator.SetBool("isWalk", isWalk);
+            playerAnimator.SetBool("isJump", isJump);
+        }
+            /*if (collision.gameObject.tag == "Item")
+            {
+                Destroy(collision.gameObject);
+                score += 10;
+            }*/
     }
 
     private void OnTriggerEnter(Collider other)
@@ -57,6 +89,7 @@ public class PlayerMovement : MonoBehaviour
         }
         if (other.gameObject.tag == "void")
         {
+            GM.GetItem(--GM.itemCount);
             GM.MoveNextStage();
         }
     }
